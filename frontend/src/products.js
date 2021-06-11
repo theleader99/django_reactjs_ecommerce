0eb28029396d815253d98c909429242b7df6530f
theleader99 <sainttroll99@gmail.com>
2021-06-11 11:13:25 +0700

    first upload

---
+++ frontend/src/products.js
@@ -0,0 +1,84 @@
+const products = [
+    {
+      '_id': '1',
+      'name': 'Milk n Honey',
+      'image': '/images/blackbook.jpg',
+      'description':
+        'a book has black color',
+      'author': 'Thang Vu',
+      'category': 'black',
+      'price': 89.99,
+      'countInStock': 10,
+      'rating': 4.5,
+      'numReviews': 12,
+    },
+    {
+      '_id': '2',
+      'name': 'Economies the UserGuide',
+      'image': '/images/bluebook.jpg',
+      'description':
+        'a book has blue color',
+      'author': 'ThangVu',
+      'category': 'blue',
+      'price': 599.99,
+      'countInStock': 0,
+      'rating': 4.0,
+      'numReviews': 8,
+    },
+    {
+      '_id': '3',
+      'name': 'Harry Potter Series',
+      'image': '/images/harrypotter.jpg',
+      'description':
+        'famous set of books',
+      'author': 'J.K.Rowling',
+      'category': 'set',
+      'price': 929.99,
+      'countInStock': 5,
+      'rating': 3,
+      'numReviews': 12,
+    },
+    {
+      '_id': '4',
+      'name': 'You r your only limit',
+      'image': '/images/redbook.jpg',
+      'description':
+        'a book has red color',
+      'author': 'QueTran',
+      'category': 'red',
+      'price': 399.99,
+      'countInStock': 11,
+      'rating': 5,
+      'numReviews': 12,
+    },
+    {
+      '_id': '5',
+      'name': 'Da Lat mot thoi huong xa',
+      'image': '/images/whitebook.jpg',
+      'description':
+        'a book has white color',
+      'author': 'VinhNguyen',
+      'category': 'white',
+      'price': 49.99,
+      'countInStock': 7,
+      'rating': 3.5,
+      'numReviews': 10,
+    },
+    {
+      '_id': '6',
+      'name': 'Dont get a Job! Make a Job',
+      'image': '/images/yellowbook.jpg',
+      'description':
+        'a book has yellow color',
+      'author': 'HungNguyenManh',
+      'category': 'yellow',
+      'price': 29.99,
+      'countInStock': 0,
+      'rating': 4,
+      'numReviews': 12,
+    },
+  ]
+  
+  
+  export default products
+  
